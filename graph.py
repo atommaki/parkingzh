8582762d3d167f76d02ef48c623787b196351f9e
Robert Kulyassa <qji@elte.hu>
2021-03-15 13:23:27 +0100

    graph.py: python2 -> python 3 (and some minor change)

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2.7
+#!/usr/bin/env python3
 
 import re
 import tempfile
@@ -6,8 +6,7 @@ import subprocess
 import os
 
 regex_int=re.compile('^[0-9]+$')
-parkingdat='parkingzh.dat'
-#parkingdat='/tmp/parkingzh.dat'
+parkingdat='data/host-0ef47f-parkingzh.dat'
 
 big_ones=['Total']
 parking_all=[]
@@ -31,22 +30,28 @@ points=1127914
 #  if i > points:
 #    points=i
 
-print 'points = '+str(points)
+print('points = '+str(points))
 
 parking_file={}
+print('big ones ', end=' ')
+print(len(big_ones))
 for parking in big_ones:
+  print('  ' + parking)
   regex_line=re.compile('.* ; '+parking+' ; .*')
   parking_file[parking]=tempfile.mktemp()
   with open(parking_file[parking],'w') as out:
     with open(parkingdat,'r') as f:
       for line in f:
         if regex_line.match(line):
-          print >>out, line,
+          print(line, end=' ', file=out)
 
 
+print('parking_all ', end=' ')
+print(len(parking_all))
 zero={}
 unknown={}
 for parking in parking_all:
+  print('  ' + parking)
   zero[parking]=0
   unknown[parking]=0
   regex_line=re.compile('.* ; '+parking+' ; .*')
@@ -62,44 +67,42 @@ for parking in parking_all:
 
 
 
-print ' Parking place                       zero        unknown'
+print(' Parking place                       zero        unknown')
 for parking in parking_all:
-  print '%30s   ' % parking,
-  print '%6i' % zero[parking],
-  print 100*zero[parking]/points
-  print '/%3i) ' % 88, '%',
-  print '/%3i) ' % int(100*zero[parking]/points), '%',
-  print '%6i' % unknown[parking],
-  print '/%3i) ' % int(100*unknown[parking]/points), '%'
+  print('%30s   ' % parking, end=' ')
+  print('%6i' % zero[parking], end=' ')
+  print(100*zero[parking]/points)
+  print('/%3i) ' % 88, '%', end=' ')
+  print('/%3i) ' % int(100*zero[parking]/points), '%', end=' ')
+  print('%6i' % unknown[parking], end=' ')
+  print('/%3i) ' % int(100*unknown[parking]/points), '%')
 
-
-exit(0)
 
 gnuplot_file=tempfile.mktemp()
 with open(gnuplot_file,'w') as gnuplot:
-  print >>gnuplot, 'set xdata time'
-  print >>gnuplot, 'set timefmt "%s"'
-  print >>gnuplot, 'set format x "%H:%M"'
-  print >>gnuplot, 'set yrange[0:]'
-  print >>gnuplot, 'set grid'
+  print('set xdata time', file=gnuplot)
+  print('set timefmt "%s"', file=gnuplot)
+  print('set format x "%H:%M"', file=gnuplot)
+  print('set yrange[0:]', file=gnuplot)
+  print('set grid', file=gnuplot)
   for i in range(1,16):
     if i > 8:
       dt='1'
     else:
       dt='3'
-    print >>gnuplot, 'set style line '+str(i)+' dt '+dt #+' lc rgb '+str(i)
-  print >>gnuplot, 'plot \\'
+    print('set style line '+str(i)+' dt '+dt, file=gnuplot) #+' lc rgb '+str(i)
+  print('plot \\', file=gnuplot)
   i=0
   for parking in big_ones:
     i=i+1
-    print >>gnuplot, '"'+parking_file[parking]+'" u 1:5  w l ls '+str(i)+' title "'+parking.replace('Parkhaus_','').replace('_',' ')+'", \\'
-  print >>gnuplot, ''
-  print >>gnuplot, 'pause -1'
+    print('"'+parking_file[parking]+'" u 1:5  w l ls '+str(i)+' title "'+parking.replace('Parkhaus_','').replace('_',' ')+'", \\', file=gnuplot)
+  print('', file=gnuplot)
+  print('pause -1', file=gnuplot)
 
 
-print ' -------------- gnuplot file --------------'
+print(' -------------- gnuplot file --------------')
 subprocess.call(['cat', gnuplot_file])
-print ' ------------------------------------------'
+print(' ------------------------------------------')
 subprocess.call(['gnuplot', gnuplot_file])
 
 for parking in big_ones:
